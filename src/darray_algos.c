52264fa1a9adca6bb64f45e953c756c1476ae24a
Aritra Sur Roy <aritra1997@hotmail.com>
2018-02-15 22:14:52 +0530

    Add heapsort and mergesort

--- src/darray_algos.c
+++ src/darray_algos.c
@@ -1,6 +1,7 @@
 #include <darray.h>
 #include <darray_algos.h>
 #include <bsd/stdlib.h>
+#include <stdlib.h>
 
 int DArray_qsort(DArray *array,DArray_compare cmp){
 	qsort(array->contents,DArray_count(array),sizeof(void *),cmp);
