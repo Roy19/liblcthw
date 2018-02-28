e05471c356e0c2b80ef4717a7f4c26ccd12b2817
Aritra Sur Roy <aritra1997@hotmail.com>
2018-02-28 21:50:21 +0530

    Add tests for Binary Search

--- tests/darray_algos_test.c
+++ tests/darray_algos_test.c
@@ -38,6 +38,24 @@ char *run_sort_test(int (*func)(DArray *,DArray_compare),const char *name){
 
 	return NULL;
 }
+char *run_search_test(){
+	int i = 0;
+	DArray *array = create_words();
+	DArray_qsort(array,(DArray_compare) testcmp);
+
+	char *test_finds[] = {"asdfasfd","xyz","oioj","ABCDEFD","DETerminant"};
+	for(i = 0;i < 5;i++){
+		debug("Searching for %s in DArray",test_finds[i]);
+		int rc = DArray_search(array,testcmp,test_finds[i]);
+		if(rc == -1){
+			debug("%s could not be found in the array",test_finds[i]);
+		}else{
+			debug("%s found in the array at position %d",test_finds[i],rc);
+		}
+	}
+	
+	return NULL;
+}
 char *test_qsort(){
 	return run_sort_test(DArray_qsort,"quicksort");
 }
@@ -51,6 +69,7 @@ char *all_tests(){
 	mu_suite_start();
 
 	mu_run_test(test_qsort);
+	mu_run_test(run_search_test);
 	//mu_run_test(test_heapsort);
 	//mu_run_test(test_mergesort);
 	
