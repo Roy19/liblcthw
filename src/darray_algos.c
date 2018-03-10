b0cb339ca761092362a93d389ee36abaae5c586a
Aritra Sur Roy <aritra1997@hotmail.com>
2018-03-10 11:28:38 +0530

    Add merge sort

--- src/darray_algos.c
+++ src/darray_algos.c
@@ -20,18 +20,18 @@
 		*a = *b;
 		*b = temp;
 	}
-	static int partition(void **array,int low,int high,DArray_compare cmp){
-		void *pivot = array[high];
+	static int partition(DArray *array,int low,int high,DArray_compare cmp){
+		void *pivot = array->contents[high];
 		int i,j = low-1;
 		for(i = low;i < high;i++){
-			if(cmp(&array[i],&pivot) <= 0){
-				swap(&array[i],&array[++j]);
+			if(cmp(&array->contents[i],&pivot) <= 0){
+				swap(&array->contents[i],&array->contents[++j]);
 			}
 		}
-		swap(&array[++j],&array[high]);
+		swap(&array->contents[++j],&array->contents[high]);
 		return j;
 	}
-	void quick_sort(void **array,int low,int high,DArray_compare cmp){
+	void quick_sort(DArray *array,int low,int high,DArray_compare cmp){
 		if(low < high){
 			int i = partition(array,low,high,cmp);
 			quick_sort(array,low,i-1,cmp);
@@ -42,12 +42,11 @@
 		check(array != NULL,"Invalid array pointers specified.");
 		check(array->contents != NULL,"Invalid array contents.");
 
-		quick_sort(array->contents,0,DArray_count(array)-1,cmp);
+		quick_sort(array,0,DArray_count(array)-1,cmp);
 		return 0;
 	error:
 		return 1;
 	}
-
 	int DArray_search(DArray *array,DArray_compare cmp,void *to_find){
 		check(array != NULL,"Invalid array pointer.");
 		check(array->contents != NULL, "Invalid contents pointer.");
@@ -66,5 +65,65 @@
 		}
 	error:	// fall through
 		return -1;
+	}
+	void merge(DArray *array,int low,int middle,int high,DArray_compare cmp){
+		void **left,**right;
+		int i,j,k;
+		left = calloc(middle-low+1,sizeof(void *));
+		right = calloc(high-middle,sizeof(void *));
+		
+		for(i = 0,k = low;i < (middle-low+1);i++,k++){
+			left[i] = array->contents[k];
+		}
+		for(j = 0,k = middle+1;j < (high-middle);j++,k++){
+			right[j] = array->contents[k];
+		}
+		i = 0;
+		j = 0;
+		k = low;
+		
+		// Standard copy, based on which is the smallest element of the left and right
+		while(i < (middle-low+1) && j < (high-middle)){
+			if(cmp(&left[i],&right[j]) <= 0){
+				array->contents[k] = left[i];
+				i++;
+			}else{
+				array->contents[k] = right[j];
+				j++;
+			}
+			k++;	
+		}
+
+		// Copy the rest of the elements
+		while(i < (middle-low+1)){
+			array->contents[k] = left[i];
+			i++;
+			k++;
+		}
+		while(j < (high-middle)){
+			array->contents[k] = right[j];
+			j++;
+			k++;
+		}
+		// Free up auxiliary space
+		free(left);
+		free(right);
+	}
+	void merge_sort(DArray *array,int low,int high,DArray_compare cmp){
+		if(low < high){
+			int middle = low + (high-low)/2;
+			merge_sort(array,low,middle,cmp);
+			merge_sort(array,middle+1,high,cmp);
+			merge(array,low,middle,high,cmp);
+		}
+	}
+	int DArray_mergesort(DArray *array,DArray_compare cmp){
+		check(array != NULL,"Invalid array pointers specified.");
+		check(array->contents != NULL,"Invalid array contents.");
+
+		merge_sort(array,0,DArray_count(array)-1,cmp);
+		return 0;
+	error:
+		return 1;
 	}
 #endif
