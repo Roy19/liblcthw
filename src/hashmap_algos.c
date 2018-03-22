a80d74bd031721280afefd47767605d6b465ff8f
Aritra Sur Roy <aritra1997@hotmail.com>
2018-03-23 00:17:17 +0530

    Add 3 other types of Hashing functions

---
+++ src/hashmap_algos.c
@@ -0,0 +1,46 @@
+#include <hashmap_algos.h>
+#include <bstrlib.h>
+
+const uint32_t FNV_PRIME = 16777619;
+const uint32_t FNV_OFFSET_BASIS = 2166136261;
+
+uint32_t Hashmap_fnv1a_hash(void *data){
+	bstring str = (bstring) data;
+	uint32_t hash = FNV_OFFSET_BASIS;
+	int i = 0;
+
+	for(i = 0;i < blength(str);i++){
+		hash ^= bchare(str,i,0);
+		hash *= FNV_PRIME;
+	}
+
+	return hash;
+}
+
+const int MOD_ADLER = 65521;
+
+uint32_t Hashmap_adler32_hash(void *data){
+	bstring str = (bstring) data;
+	uint32_t a = 0,b = 1;
+	int i = 0;
+
+	for(i = 0;i < blength(str);i++){
+		a = (a + bchare(str,i,0)) % MOD_ADLER;
+		b = (b + a) % MOD_ADLER;
+	}
+
+	return (b << 16) | a;
+}
+
+uint32_t Hashmap_djb_hash(void *data){
+	bstring str = (bstring) data;
+	uint32_t hash = 5381;
+	int i = 0;
+
+	for(i = 0;i < blength(str);i++){
+		hash = ((hash << 5) + hash) + bchare(str,i,0); /* hash = hash*33 + c */
+	}
+
+	return hash;
+}
+
