abb2d3a39a885eb4393786da3d451865e7873e51
Aritra Sur Roy <aritra1997@hotmail.com>
2018-06-07 01:37:37 +0530

    Add RingBuffer module and tests for it

---
+++ tests/ringbuffer_test.c
@@ -0,0 +1,47 @@
+#include "minunit.h"
+#include <bstrlib.h>
+#include <ringbuffer.h>
+
+struct tagbstring test1 = bsStatic("test1");
+struct tagbstring test2 = bsStatic("test2");
+char store[20];
+RingBuffer *buffer;
+
+char *test_create(){
+	buffer = RingBuffer_create(blength(&test1) + blength(&test2));
+	mu_assert(buffer != NULL, "Failed to create RingBuffer.");
+
+	return NULL;
+}
+char *test_read_write(){
+	int ret;
+	ret = RingBuffer_write(buffer,bdata(&test1),blength(&test1));
+	mu_assert(ret == blength(&test1), "Could not write test1 to RingBuffer.");
+	
+	ret = RingBuffer_read(buffer,store,blength(&test1));
+	mu_assert(ret == blength(&test1), "Invalid # of characters read from RingBuffer.");	
+	
+	ret = RingBuffer_write(buffer,bdata(&test2),blength(&test2));
+	mu_assert(ret == blength(&test2),
+			"Invalid # of characters written to RingBuffer at 2nd attempt.");
+	ret = RingBuffer_read(buffer,store + blength(&test1),blength(&test2));
+	mu_assert(ret == blength(&test2), "Invalid # of characters read at 2nd read attempt.");
+
+	return NULL;
+}
+char *test_destroy(){
+	RingBuffer_destroy(buffer);
+
+	return NULL;
+}
+char *all_tests(){
+	mu_suite_start();
+
+	mu_run_test(test_create);
+	mu_run_test(test_read_write);
+	mu_run_test(test_destroy);
+	
+	return NULL;
+}
+
+RUN_TESTS(all_tests);
