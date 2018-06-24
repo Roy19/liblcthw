354a7fde321a7bc197d3994fc5cb448dfa30a46a
Aritra Sur Roy <aritra1997@hotmail.com>
2018-06-24 12:15:35 +0530

    Moved sources to lcthw folder

---
+++ src/lcthw/queue.h
@@ -0,0 +1,17 @@
+#ifndef lcthw_QUEUE_H_
+#define lcthw_QUEUE_H_
+
+#include <lcthw/list.h>
+
+typedef List Queue;
+
+#define Queue_create    List_create
+#define Queue_destroy  List_destroy
+#define Queue_send      List_push
+#define Queue_recv      List_shift
+#define Queue_peek      List_first
+#define Queue_count     List_count
+
+#define QUEUE_FOREACH(queue,cur)        LIST_FOREACH(queue,last,prev,cur)
+
+#endif
